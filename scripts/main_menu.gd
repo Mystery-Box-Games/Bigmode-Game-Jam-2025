272e8fd0b840d5797b5b121cca26ef9f057ead4a
Danpantzz <danpantz@gmail.com>
2025-01-24 18:55:48 -0500

    added credits

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -16,5 +16,9 @@ func _on_button_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
 
 
+func _on_button_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits_menu.tscn")
+
+
 func _on_button_quit_pressed() -> void:
 	get_tree().quit()
