471a9f201a1cf655b74c0bc7930e1f4c554b10d8
Danpantzz <danpantz@gmail.com>
2025-01-25 02:06:04 -0500

    added some themes the correct way

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$ColorRect/VBoxContainer/VolumeSlider.grab_focus()
+	$CenterContainer/PanelContainer/VBoxContainer/VolumeSlider.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
