08f96df042aec976e02d9ce10e268e559b60f68b
Danpantzz <danpantz@gmail.com>
2025-01-29 16:02:49 -0500

    added player ui

---
+++ scripts/ui.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+@onready var health_node = get_tree().root.get_node("Game").get_node("Player").get_node("Health")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	health_node.health = clamp(health_node.health, 0, 100)
+	$ProgressBar.value = health_node.health
