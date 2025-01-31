8a8eaf368430bfbd92daf06f7dfbb85a98733b10
Danpantzz <danpantz@gmail.com>
2025-01-30 20:31:05 -0500

    added a timer to show how long it will be before the brain power runs out

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,14 +2,37 @@ extends CharacterBody2D
 
 class_name Player
 
+enum Tiers {BASE, ONE, TWO, THREE, FOUR, FIVE}
+var tier: Tiers = Tiers.BASE
+
 const SPEED := 3.0
 
 var brain_power = 0
+var prev_brain_power = 0
+var brain_tier_index = 0
+var brain_tier = {
+	0: Tiers.BASE,
+	1: Tiers.ONE,
+	2: Tiers.TWO,
+	3: Tiers.THREE,
+	4: Tiers.FOUR,
+	5: Tiers.FIVE
+}
 
 @onready var weapon_socket: Node2D = $WeaponSocket
+@onready var brain_power_timer: Timer = $BrainPowerTimer
+
 
 func _physics_process(delta: float) -> void:
-	#look_at(get_global_mouse_position())
+	brain_tier_index = clampi(brain_tier_index, 0, 5)
+	brain_power = clampi(brain_power, 0, 50)
+	
+	if (prev_brain_power < brain_power):
+		prev_brain_power = brain_power
+		brain_power_timer.start()
+	if (brain_power > 0 && brain_power_timer.is_stopped()):
+		print("starting timer")
+		brain_power_timer.start()
 	
 	# get direction from keyboard
 	var character_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -30,3 +53,24 @@ func _physics_process(delta: float) -> void:
 		weapon_socket.position.x = 9
 		
 	move_and_slide()
+
+func _on_brain_power_timer_timeout() -> void:
+	brain_tier_index -= 1
+	brain_tier_index = clampi(brain_tier_index, 0, 5)
+	set_state(brain_tier[brain_tier_index])
+
+func set_state(state: Tiers) -> void:
+	match state:
+		Tiers.BASE:
+			pass
+		Tiers.ONE:
+			pass
+		Tiers.TWO:
+			pass
+		Tiers.THREE:
+			pass
+		Tiers.FOUR:
+			pass
+		Tiers.FIVE:
+			pass
+	
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,7 +1,13 @@
 extends CanvasLayer
 
 @onready var game = get_tree().root.get_node("Game").get_node("GameManager")
+@onready var player = get_tree().root.get_node("Game").get_node("Player")
 @onready var health_node = get_tree().root.get_node("Game").get_node("Player").get_node("Health")
+@onready var round_text: Label = $RoundText
+@onready var brain_text: Label = $VBoxContainer/BrainText
+@onready var brain_progress: TextureProgressBar = $VBoxContainer/HBoxContainer/BrainProgress
+@onready var reset_progress: ProgressBar = $VBoxContainer/HBoxContainer/ResetProgress
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,4 +19,9 @@ func _process(delta: float) -> void:
 	health_node.health = clamp(health_node.health, 0, 100)
 	$ProgressBar.value = health_node.health
 	
-	$Label.text = "Round %s" % (game.round + 1)
+	brain_progress.value = player.brain_power
+	brain_text.text = "Tier: %s" % player.brain_tier_index
+	
+	reset_progress.value = (player.get_node("BrainPowerTimer").get_time_left() / player.get_node("BrainPowerTimer").get_wait_time()) * 100
+	
+	round_text.text = "Round %s" % (game.round + 1)
