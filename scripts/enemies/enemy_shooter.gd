3aba021c736223ab70073b68e91cd9000ff6a9c3
Danpantzz <danpantz@gmail.com>
2025-02-01 16:55:05 -0500

    updated enemy_shooter gun sprite, enemy_shooter walk animation, and the enemy_brute now attacks faster

--- scripts/enemies/enemy_shooter.gd
+++ scripts/enemies/enemy_shooter.gd
@@ -3,6 +3,8 @@ extends "res://scripts/enemies/enemy.gd"
 @export var bullet_scn: PackedScene
 @export var bullet_speed: float = 1200
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 @onready var enemy_shooter_gun: Node2D = $WeaponSocket/EnemyShooterGun
 @onready var weapon_socket: Node2D = $WeaponSocket
 
@@ -32,12 +34,12 @@ func _process(delta: float) -> void:
 	
 func _on_attack_range_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Player:
-		print("player in range")
+		animation_player.play("idle")
 		within_attack_range = true
 
 func _on_attack_range_area_exited(area: Area2D) -> void:
 	if area.get_parent() is Player:
-		print("player out of range")
+		animation_player.play("walk")
 		within_attack_range = false
 		time_until_attack = attack_speed
 
