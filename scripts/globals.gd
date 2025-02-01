b3d74b82f03d46c77fd839b9cb8770c823f38f7a
Danpantzz <danpantz@gmail.com>
2025-02-01 16:13:40 -0500

    added a score variable to globals

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -9,10 +9,11 @@ var options_scene = preload("res://scenes/menus/options_menu.tscn")
 var credits_scene = preload("res://scenes/menus/credits_menu.tscn")
 
 var game_over: bool = false
-
+var score: int
 var enemies_defeated = 0
 
 func reset():
+	score = 0
 	prevscene = null
 	game_over = false
 	enemies_defeated = 0
