03d2f3f6a36ae632ace440d81af672e0d8fbf832
Danpantzz <danpantz@gmail.com>
2025-02-02 23:34:56 -0500

    added music and sfx sliders

--- scripts/menus/options_menu.gd
+++ scripts/menus/options_menu.gd
@@ -1,14 +1,24 @@
 extends Control
 
+@export var master_bus_name := "Master"
+@export var music_bus_name := "Music"
+@export var sfx_bus_name := "SFX"
+
+@onready var master_bus := AudioServer.get_bus_index(master_bus_name)
+@onready var music_bus := AudioServer.get_bus_index(music_bus_name)
+@onready var sfx_bus := AudioServer.get_bus_index(sfx_bus_name)
+
+@onready var master_slider: HSlider = $TabContainer/Audio/VBoxContainer/MasterSlider
+@onready var music_slider: HSlider = $TabContainer/Audio/VBoxContainer/MusicSlider
+@onready var sfx_slider: HSlider = $TabContainer/Audio/VBoxContainer/SFXSlider
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#$CenterContainer/PanelContainer/VBoxContainer/VolumeSlider.grab_focus()
-	pass
+	master_slider.value = db_to_linear(AudioServer.get_bus_volume_db(master_bus))
+	music_slider.value = db_to_linear(AudioServer.get_bus_volume_db(music_bus))
+	sfx_slider.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus))
 	
-func _on_option_button_item_selected(index: int) -> void:
-	var size = $TabContainer/Display/HBoxContainer/OptionButton.get_item_text(index).split("x")
-	get_viewport().size = Vector2(float(size[0]), float(size[1]))
 
 func _on_check_button_toggled(toggled_on: bool) -> void:
 	if (toggled_on):
@@ -18,3 +28,21 @@ func _on_check_button_toggled(toggled_on: bool) -> void:
 
 func _on_button_back_pressed() -> void:
 	get_tree().change_scene_to_file(Globals.prevscene)
+
+func _on_master_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(master_bus, linear_to_db(value))
+	
+func _on_master_slider_mouse_exited() -> void:
+	release_focus()
+
+func _on_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(music_bus, linear_to_db(value))
+
+func _on_music_slider_mouse_exited() -> void:
+	release_focus()
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(value))
+
+func _on_sfx_slider_mouse_exited() -> void:
+	release_focus()
